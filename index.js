690b4e99a57af8cc8385876573c415f1827eb438
sindhuja2306 <pailasindhuja102@gmail.com>
2024-11-30 16:55:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const textBox=document.getElementById("textBox");
+const toFarnaheit=document.getElementById("toFarnaheit");
+const toCelsius=document.getElementById("toCelsius");
+const result=document.getElementById("result");
+let temp;
+function convert(){
+
+    if(toFarnaheit.checked){
+         temp=Number(textBox.value);
+         temp=temp*9/5 +32;
+         result.textContent=temp.toFixed(1)+"°F";
+    }else if(toCelsius.checked){
+         temp=Number(textBox.value);
+         temp=(temp-32)*(5/9);
+         result.textContent=temp.toFixed(1)+"°C"
+    }else{
+         result.textContent="select a unit";
+    }
+}
